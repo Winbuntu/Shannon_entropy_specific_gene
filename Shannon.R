e9b9686ff5a6d0fa766915c8fad6669a32b45cf9
no zou do die why you try <anqin@s-164-67-237-56.resnet.ucla.edu>
2016-09-28 21:58:35 -0700

    add analysis

--- Shannon.R
+++ Shannon.R
@@ -4,6 +4,11 @@ Stage.fpkm.table = read.table("GSE66582_stage_fpkm.txt",head=T,row.names = 1)
 
 
 
+# using Shannon entropy and Q statistics, ideas and formula are come from these papers:
+# 1.Promoter features related to tissue specificity as measured by Shannon entropy (Q stst)
+# 2.ROKU: a novel method for identification of tissue-specific genes (Shannon)
+
+
 Pt.g.normalize <- function(x) {
   
   # normalize gene expression level. 
@@ -64,7 +69,60 @@ Q.gt.matrix.compute <- function(FPKM.table){
 ##############################
 ## following are analysis part.
 
+# we use these method to identify stage specific genes using data from:
+# The landscape of accessible chromatin in mammalian preimplantation embryos
+###  GSE66582, this is scRNA. This data file is from this record
 
 Q.stat.matrix = Q.gt.matrix.compute(Stage.fpkm.table+0.01)
+
+
+### find non-maternal stage specific genes
+# oocytes (FPKM ≤ 0.5) but that are activated (FPKM>1) after ZGA
+
+not.expressed.in.oocyte = Stage.fpkm.table$MII_oocyte <=0.5
+expressed.later.than.oocyte = (apply(Stage.fpkm.table[,c(3: (ncol(Stage.fpkm.table)-1) )],1,max) > 1)
+
+early.2.cell.Q.stst = (Q.stat.matrix[,3]<2)
+x2.cell.Q.stat = (Q.stat.matrix[,4]<2)
+x4.cell.Q.stat = (Q.stat.matrix[,5]<2)
+x8.cell.Q.stat = (Q.stat.matrix[,6]<2)
+mESC.cell.Q.stat = (Q.stat.matrix[,7]<2)
+
+early.2.cell.list = not.expressed.in.oocyte & expressed.later.than.oocyte & early.2.cell.Q.stst &
+  (Stage.fpkm.table$early_2cell>=3)
+
+x2.cell.list = not.expressed.in.oocyte & expressed.later.than.oocyte & x2.cell.Q.stat &
+  (Stage.fpkm.table$X2cell>=3)
+
+x4.cell.list = not.expressed.in.oocyte & expressed.later.than.oocyte & x4.cell.Q.stat &
+  (Stage.fpkm.table$X4cell>=3)
+
+x8.cell.list = not.expressed.in.oocyte & expressed.later.than.oocyte & x8.cell.Q.stat &
+  (Stage.fpkm.table$X8cell>=3)
+
+
+
+color.palette = colorRampPalette(c("midnightblue","dodgerblue3","white","goldenrod1","darkorange2"), space="Lab")
+#pdf(file="./figs/heatmap.pdf",height=10,width=10)
+heatmap.2(as.matrix((Stage.fpkm.table[ as.vector(na.omit(early.2.cell.list | x2.cell.list|
+                                                           x4.cell.list | x8.cell.list)),] )),
+          trace="none",density="none",
+          Colv = NA,
+          key=T,scale="row",
+          
+          dendrogram="both",
+          col=color.palette,
+          margins=c(7,6)
+
+)
+
+# using shannon directly, without computing Q statistics
+
+
+hg.stat = apply(Stage.fpkm.table+0.01,1,Hg.compute)
+
+
+
+
 
 
